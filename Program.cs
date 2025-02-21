e4884f90538d60b900d219b940d5ea8ba02b7eae
VuKhoi219 <ellyproe@gmail.com>
2025-02-21 10:17:10 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -34,15 +34,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-if (args.Contains("--seed"))
-{
-    using (var scope = app.Services.CreateScope())
-    {
-        var dbContext = scope.ServiceProvider.GetRequiredService<MyDbContext>();
-        DatabaseSeeder.Seed(dbContext);
-        Console.WriteLine("✅ Dữ liệu InsuranceVehicleDetail đã được seed!");
-    }
-}
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
