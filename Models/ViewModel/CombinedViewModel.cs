c9167391eab8ffcab12c4f4ab3bf9e884de83ce4
vukhoi2k5 <vukhoi353@gmail.com>
2025-02-26 14:49:34 +0700

    add view model

---
+++ Models/ViewModel/CombinedViewModel.cs
@@ -0,0 +1,8 @@
+namespace Project_Sem3.Models.ViewModel;
+
+public class CombinedViewModel
+{
+  public NavbarViewModel Navbar { get; set; }
+  public PageHeaderViewModel PageHeader { get; set; }
+}
+
